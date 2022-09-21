b8d9270ea7fb10443c3edbd20ebfc1f22ca19471
devinh2401 <89353024+devinh2401@users.noreply.github.com>
2022-09-22 01:00:55 +0700

    1pm home

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Homepage from './Components/Homepage';
+
+
+function App() {
+  return (
+   <Router>
+      <Routes>
+          <Route exact  path="/" element={<Homepage />} />
+      </Routes>
+   </Router>
+  );
+}
+
+export default App;
